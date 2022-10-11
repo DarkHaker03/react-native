d883d3f582dbe2229bcfe53511316b173e2841d1
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-10-12 00:35:15 +0500

    delete: deleted unusable comments

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,18 +5,3 @@ export default function App() {
     <Pages />
   )
 }
-
-// const newData = await (await axios.get<UsersArguments[]>('https://jsonplaceholder.typicode.com/users')).data;
-      // const newData2 = await (await axios.get<PostArguments[]>('https://jsonplaceholder.typicode.com/posts')).data;
-      // setData(newData);
-      // setData2(newData2);
-      // fetch('https://jsonplaceholder.typicode.com/users')
-      //   .then((response) => response.json())
-      //   .then((response) => {
-      //     setData(response);
-      //   });
-      // fetch('https://jsonplaceholder.typicode.com/posts')
-      //   .then((response) => response.json())
-      //   .then((response) => {
-      //     setData2(response);
-      //   });
